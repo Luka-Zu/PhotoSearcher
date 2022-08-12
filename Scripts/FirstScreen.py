d1d3f5178f2cc152ec59403deb94937cd0884f94
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-12 13:33:53 +0400

    Finished search method

--- Scripts/FirstScreen.py
+++ Scripts/FirstScreen.py
@@ -1,6 +1,26 @@
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import *
+import wikipedia
+import requests
 
 
 class FirstScreen(Screen):
+
     def search(self):
-        pass
+        # Get user query from text input
+        query = self.manager.current_screen.ids.user_query.text
+
+        # Get wikipedia page and first image URL
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+
+        print(image_link)
+
+        # Download the Image
+
+        req = requests.get(image_link, stream=True)
+
+        with open("G.jpg", 'wb') as file:
+
+            file.write(req.content)
+
+        self.manager.current_screen.ids.img.source = 'G.jpg'
